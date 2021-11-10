2caec63fcc6fb0e511225b1029d95eeaa16711a3
PavsterUK <pavelnaumovic@gmail.com>
2021-11-10 21:43:59 +0000

    initial

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <img alt="card" srs='' />
+            <div>
+                <h2>John Doe</h2>
+                <p>jonedoe@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
