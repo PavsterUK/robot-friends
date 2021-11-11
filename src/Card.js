66dd9941baa1af0ea938693ce7791c3663bc9e00
PavsterUK <pavelnaumovic@gmail.com>
2021-11-11 22:11:11 +0000

    add searchbox

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ name, email, id}) => {
     return (
-        <div>
-            <img alt="card" srs='' />
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="robots" src={`https://robohash.org/${id}`} />
             <div>
-                <h2>John Doe</h2>
-                <p>jonedoe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
